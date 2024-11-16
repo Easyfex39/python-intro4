79a1ecd78f791e03ab8b6f39bf0fda0e2a594134
easyfex <idahosaefosa19@gmail.com>
2024-11-16 07:50:33 -0500

    python intro4

---
+++ area.py
@@ -0,0 +1,14 @@
+#write a python code that will calculate the area rectangle of length 10meter breadth 8meter
+
+l=10
+b=8
+Area=l*b
+print(Area)
+
+#calculate a perimeter of a rectangle
+perimeter=(l+b)*2
+print(perimeter)
+
+d=Area-perimeter
+print(d)
+    
---
+++ userinputer.py
@@ -0,0 +1,8 @@
+# write a python code that will collect your surname
+print("enter your surname")
+surname=input()
+print("enter your first name")
+firstname=input
+print("enter your phone number")
+phonenumber=input()
+print("details" , surname , firstname ,phonenumber)
